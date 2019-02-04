7044b8e0bdd25f513606ba55fbbe3fcd0f4d6f9a
aydanszr <aydanszrm@gmail.com>
2019-02-05 00:33:00 +0600

    first week

---
+++ week1/Task2/task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace task1
+{
+    class Student
+    {
+        public string name;
+        public string id;
+        public int year = 0;
+        public Student(string name, string id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+        public void PrintInfo()
+        {
+            year++;
+
+            Console.WriteLine(name + " " + id + " " + year);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student("Aydan", "18BD110875");
+            s.PrintInfo();
+        }
+    }
+}
