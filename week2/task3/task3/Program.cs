1ae5c5d8d7a336cd2b0bb56162df087201b4cafd
aydanszr <aydanszrm@gmail.com>
2019-02-07 14:33:46 +0600

    Second Week

---
+++ week2/task3/task3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace task3
+{
+    class Program
+    {
+        static void PrintSpaces(int level)
+        {
+            for (int i = 0; i < level; i++)
+                Console.Write("  ");
+        }
+
+        static void task(DirectoryInfo directory, int level)
+        {
+            FileInfo[] files = directory.GetFiles();
+            DirectoryInfo[] directories = directory.GetDirectories();
+
+            foreach (FileInfo file in files)
+            {
+                PrintSpaces(level);
+                Console.WriteLine(file.Name);
+            }
+
+            foreach (DirectoryInfo d in directories)
+            {
+                PrintSpaces(level);
+                Console.WriteLine(d.Name);
+                task(d, level + 1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo("/Users/aydan/desktop/c#");
+            task(d, 1);
+        }
+    }
+}
