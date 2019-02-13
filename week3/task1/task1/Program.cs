31f4edee7404848fbf0bd4de750c0b43bf7b11a0
aydanszr <aydanszrm@gmail.com>
2019-02-14 01:37:45 +0600

    not ready

---
+++ week3/task1/task1/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.IO;
+
+namespace Task1
+{
+    class FarManager
+    {
+        DirectoryInfo dir = null;
+        public int cursor;
+        public string path;
+        public int size;
+        FileSystemInfo f1 = null;
+        public FarManager()
+        {
+
+        }
+        public FarManager(string path)
+        {
+            this.path = path;
+            cursor = 0;
+        }
+        public void Color(FileSystemInfo f, int index)
+        {
+            if (cursor == index)
+            {
+                Console.BackgroundColor = ConsoleColor.Magenta;
+                Console.ForegroundColor = ConsoleColor.Black;
+                f1 = f;
+            }
+            else if (f.GetType() == typeof(DirectoryInfo))
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+        }
+        public void Show()
+        {
+
+            dir = new DirectoryInfo(path);
+            FileSystemInfo[] FSI = dir.GetFileSystemInfos();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            for (int k = 0, j = 0; k < FSI.Length; k++)
+            {
+                if (FSI[k].Name[0] == '.')
+                    continue;
+                Color(FSI[k], j);
+                Console.WriteLine(j + 1 + ". " + FSI[k].Name);
+                j++;
+            }
+
+        }
+        public void CalSize()
+        {
+            DirectoryInfo d = new DirectoryInfo(path);
+            FileSystemInfo[] fi = d.GetFileSystemInfos();
+            size = fi.Length;
+            for (int k = 0; k < fi.Length; k++)
+            {
+                if (fi[k].Name[0] == '.')
+                    size--;
+            }
+        }
+        public void Down()
+        {
+            cursor++;
+            if (cursor == size)
+                cursor = 0;
+        }
+        public void Up()
+        {
+            cursor--;
+            if (cursor < 0)
+                cursor = size - 1;
+        }
+        public void Start()
+        {
+
+            ConsoleKeyInfo Cons;
+            bool Ok = true;
+            while (Ok == true)
+            {
+                CalSize();
+                Show();
+                Cons = Console.ReadKey();
+                Console.Clear();
+
+                if (Cons.Key == ConsoleKey.DownArrow)
+                {
+                    Down();
+                }
+                else if (Cons.Key == ConsoleKey.Spacebar)
+                {
+                    Ok = false;
+                }
+                else if (Cons.Key == ConsoleKey.UpArrow)
+                {
+                    Up();
+                }
+                else if (Cons.Key == ConsoleKey.Enter)
+                {
+                    if (f1.GetType() == typeof(DirectoryInfo))
+                    {
+                        cursor = 0;
+                        path = f1.FullName;
+                    }
+                    else
+                    {
+                        StreamReader SR = new StreamReader(f1.FullName);
+                        Console.WriteLine(SR.ReadToEnd());
+                        SR.Close();
+                        Console.ReadKey();
+                        Console.Clear();
+
+                    }
+                }
+                else if (Cons.Key == ConsoleKey.Escape)
+                {
+                    if (dir.Parent.FullName != "/Users/aydan")
+                    {
+                        path = dir.Parent.FullName;
+                        cursor = 0;
+                        Console.Clear();
+                    }
+                    else
+                    {
+                        Console.WriteLine("you cannot do this");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+                }
+                else if (Cons.Key == ConsoleKey.Backspace)
+                {
+                    if (f1.GetType() == typeof(DirectoryInfo))
+                    {
+                        cursor = 0;
+                        Directory.Delete(f1.FullName);
+                    }
+                    else
+                    {
+                        cursor = 0;
+                        File.Delete(f1.FullName);
+                    }
+                }
+                else if (Cons.Key == ConsoleKey.Tab)
+                {
+                    Console.Clear();
+                    string name = Console.ReadLine();
+                    Console.Clear();
+                    string copPath = Path.Combine(dir.FullName, name);
+                    if (f1.GetType() == typeof(DirectoryInfo))
+                    {
+                        Directory.Move(f1.FullName, copPath);
+                    }
+                    else
+                    {
+                        File.Move(f1.FullName, copPath);
+                    }
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "/Users/aydan/Desktop/c++";
+            FarManager FM = new FarManager(path);
+            FM.Start();
+        }
+    }
+}
