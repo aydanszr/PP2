26ec0f8150b6dba7795baeb03f5a10ed0eb7ea44
aydanszr <aydanszrm@gmail.com>
2019-02-15 19:12:16 +0600

    ex

---
+++ ex/ex/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ex
+{
+    class triangle
+    {
+        private int a;
+        private int b;
+        private int c;
+
+        public triangle(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+
+        }
+        public int getPerimetr()
+        {
+            return a + b + c;
+        }
+
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                triangle t = new triangle(3, 4, 5);
+                Console.WriteLine(t.getPerimetr());
+
+            }
+        }
+    }
+}
